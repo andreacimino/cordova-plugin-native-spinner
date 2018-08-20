1b3d9cb2bcb0b8cc933132dc4afe82390285773c
andreacimino <andreacimino@gmail.com>
2018-08-20 12:06:04 +0200

    Update CDVSpinnerDialog.m

--- src/ios/CDVSpinnerDialog.m
+++ src/ios/CDVSpinnerDialog.m
@@ -12,6 +12,7 @@
     NSString *callbackId;
     NSString *title;
     NSString *message;
+    NSString *content;
     NSNumber *isFixed;
     NSString *alpha;
     NSString *red;
@@ -36,7 +37,12 @@
     if ((NSFoundationVersionNumber <= 1047.25 /* 7.1 */) && UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
         return CGRectMake( 0.0f, 0.0f, [[UIScreen mainScreen]bounds].size.height, [UIScreen mainScreen].bounds.size.width);
     }
-    return CGRectMake( 0.0f, 0.0f, [[UIScreen mainScreen]bounds].size.width, [UIScreen mainScreen].bounds.size.height);
+    float screen_width = [[UIScreen mainScreen]bounds].size.width;
+    float screen_height = [UIScreen mainScreen].bounds.size.height;
+
+    float width = [[UIScreen mainScreen]bounds].size.width/1.8;
+    float height = [UIScreen mainScreen].bounds.size.height/7.5;
+    return CGRectMake( (screen_width-width)/2, (screen_height -height)/2, width, height);
 }
 
 - (void)handleTapGesture:(UITapGestureRecognizer *)gesture
@@ -53,22 +59,36 @@
 - (UIView *)overlay {
     if (!_overlay) {
         _overlay = [[UIView alloc] initWithFrame:self.rectForView];
-        _overlay.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:[alpha floatValue]];
+        _overlay.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.6];
+        //_indicator.fr
         _indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
-        _indicator.center = _overlay.center;
+        
+        float screen_width = [[UIScreen mainScreen]bounds].size.width;
+        float screen_height = [UIScreen mainScreen].bounds.size.height;
+        
+        float width = [[UIScreen mainScreen]bounds].size.width/1.8;
+        float height = [UIScreen mainScreen].bounds.size.height/7.5f;
+        int spinner_width = 15;
+        _indicator.frame = CGRectMake((width/2) -spinner_width, ((height/2)/2)-8, _indicator.frame.size.width, _indicator.frame.size.height);
+
+    //     _indicator.ce = _overlay.center;
+       // indicator.center  = CGPointMake(800, 800);
+     //   indicator.frame = self.rectForView;
         [_indicator startAnimating];
         [_overlay addSubview:_indicator];
+       CGRect rect = CGRectMake(0, 0, width, (1.5*height)+7);
 
-        _messageView = [[UILabel alloc] initWithFrame: self.rectForView];
-        [_messageView setText: message == nil ? title : message];
-        [_messageView setTextColor: [UIColor colorWithRed:[red floatValue] green:[green floatValue] blue:[blue floatValue] alpha:0.65]];
+      _messageView = [[UILabel alloc] initWithFrame: rect];
+        [_messageView setText: content];
+        [_messageView setTextColor: UIColor.whiteColor];
         [_messageView setBackgroundColor: [UIColor colorWithRed:0 green:0 blue:0 alpha:0]];
         [_messageView setTextAlignment: NSTextAlignmentCenter];
-        _messageView.center = (CGPoint){_overlay.center.x, _overlay.center.y + 40};
-        _messageView.font = [UIFont fontWithName:@"Helvetica" size:(10.0)];
+    //    _messageView.center = (CGPoint){_overlay.center.x, _overlay.center.y + 40};
+        _messageView.font = [UIFont fontWithName:@"Helvetica" size:(14.0)];
         _messageView.lineBreakMode = UILineBreakModeWordWrap;
         _messageView.numberOfLines = 0;
         [_overlay addSubview:_messageView];
+ 
 
         UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
         [_overlay addGestureRecognizer: tapRecognizer];
@@ -83,11 +103,8 @@
 
     title = [command argumentAtIndex:0];
     message = [command argumentAtIndex:1];
-    isFixed = [command argumentAtIndex:2];
-    alpha = [command argumentAtIndex:3];
-    red = [command argumentAtIndex:4];
-    green = [command argumentAtIndex:5];
-    blue = [command argumentAtIndex:6];
+    content = [command argumentAtIndex:2];
+    isFixed = [command argumentAtIndex:3];
     
     UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 
